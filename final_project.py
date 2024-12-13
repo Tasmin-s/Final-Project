7721e44220a08c242b3d2fab31ade52e095eaed3
tasmin-s <tasmin1002@gmail.com>
2024-12-13 15:23:59 +0000

    test

--- final_project.py
+++
@@ -1,2 +0,0 @@
-def Final_Project():
-    print("Hello world")
---
+++ function.py
@@ -0,0 +1,7 @@
+def multiply_two_numbers(a,b):
+    if (type(a) != int) or (type(b) != int):
+        raise TypeError('Please input a number not a string')
+    return a * b
+
+
+
